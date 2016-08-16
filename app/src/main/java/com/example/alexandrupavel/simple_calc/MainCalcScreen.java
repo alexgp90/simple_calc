3e528bde481ca14a41e3c83169b604c8d203e558
alexgp90 <C0deofarms>
2016-08-16 09:33:00 +0300

    Initial commit

---
+++ app/src/main/java/com/example/alexandrupavel/simple_calc/MainCalcScreen.java
@@ -0,0 +1,13 @@
+package com.example.alexandrupavel.simple_calc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainCalcScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_calc_screen);
+    }
+}
