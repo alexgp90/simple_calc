8505a58df315e2e380c784e6612094f4c4256bec
alexgp90 <C0deofarms>
2016-08-17 13:55:36 +0300

    sum and equals are borked

--- app/src/main/java/com/example/alexandrupavel/simple_calc/MainCalcScreen.java
+++ app/src/main/java/com/example/alexandrupavel/simple_calc/MainCalcScreen.java
@@ -3,6 +3,7 @@ package com.example.alexandrupavel.simple_calc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,6 +14,7 @@ public class MainCalcScreen extends AppCompatActivity {
 
     long result=0;
     long val_temp=0;
+    boolean operator = false;
 
 
     @Override
@@ -21,12 +23,19 @@ public class MainCalcScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main_calc_screen);
 
         final TextView results_field = (TextView) findViewById(R.id.results_field);
-        results_field.setText("");
+        results_field.setText("0");
 
 
         final Button button_0 = (Button) findViewById(R.id.button_0);
         button_0.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                if (operator == true){
+                    operator = false;
+                    results_field.setText("0");
+                }
+                if (results_field.getText().toString().equals("0")){
+                    results_field.setText("");
+                }
                results_field.append("0");
 
             }
@@ -34,68 +43,153 @@ public class MainCalcScreen extends AppCompatActivity {
         final Button button_1 = (Button) findViewById(R.id.button_1);
         button_1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                if (operator == true){
+                    operator = false;
+                    results_field.setText("0");
+                }
+                if (results_field.getText().toString().equals("0")){
+                    results_field.setText("");
+                }
                 results_field.append("1");
             }
         });
         final Button button_2 = (Button) findViewById(R.id.button_2);
         button_2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                if (operator == true){
+                    operator = false;
+                    results_field.setText("0");
+                }
+                if (results_field.getText().toString().equals("0")){
+                    results_field.setText("");
+                }
                 results_field.append("2");
             }
         });
         final Button button_3 = (Button) findViewById(R.id.button_3);
         button_3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                if (operator == true){
+                    operator = false;
+                    results_field.setText("0");
+                }
+                if (results_field.getText().toString().equals("0")){
+                    results_field.setText("");
+                }
                 results_field.append("3");
             }
         });
         final Button button_4 = (Button) findViewById(R.id.button_4);
         button_4.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                if (operator == true){
+                    operator = false;
+                    results_field.setText("0");
+                }
+                if (results_field.getText().toString().equals("0")){
+                    results_field.setText("");
+                }
                 results_field.append("4");
             }
         });
         final Button button_5 = (Button) findViewById(R.id.button_5);
         button_5.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                if (operator == true){
+                    operator = false;
+                    results_field.setText("0");
+                }
+                if (results_field.getText().toString().equals("0")){
+                    results_field.setText("");
+                }
+                if (results_field.getText().toString().equals("0")){
+                    results_field.setText("");
+                }
                 results_field.append("5");
             }
         });
         final Button button_6 = (Button) findViewById(R.id.button_6);
         button_6.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                if (operator == true){
+                    operator = false;
+                    results_field.setText("0");
+                }
+                if (results_field.getText().toString().equals("0")){
+                    results_field.setText("");
+                }
                 results_field.append("6");
             }
         });
         final Button button_7 = (Button) findViewById(R.id.button_7);
         button_7.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                if (operator == true){
+                    operator = false;
+                    results_field.setText("0");
+                }
+                if (results_field.getText().toString().equals("0")){
+                    results_field.setText("");
+                }
                 results_field.append("7");
             }
         });
         final Button button_8 = (Button) findViewById(R.id.button_8);
         button_8.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                if (operator == true){
+                    operator = false;
+                    results_field.setText("0");
+                }
+                if (results_field.getText().toString().equals("0")){
+                    results_field.setText("");
+                }
                 results_field.append("8");
             }
         });
         final Button button_9 = (Button) findViewById(R.id.button_9);
         button_9.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                if (operator == true){
+                    operator = false;
+                    results_field.setText("0");
+                }
+                if (results_field.getText().toString().equals("0")){
+                    results_field.setText("");
+                }
+
                 results_field.append("9");
             }
         });
         final Button button_clear = (Button) findViewById(R.id.button_clear);
         button_clear.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                results_field.setText("");
+
+                    operator = false;
+                    results_field.setText("0");
+                    result = 0;
+
             }
         });
 
         final Button button_sum = (Button) findViewById(R.id.button_sum);
-        button_clear.setOnClickListener(new View.OnClickListener() {
+        button_sum.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                results_field.setText("");
+                if (operator == false) {
+                    operator = true;
+                    result = result + Long.parseLong(results_field.getText().toString());
+                    Log.d("tag_true",Long.toString(result));
+                    results_field.setText("0");
+                } else
+                if (operator == true){
+
+                    result = result + Long.parseLong(results_field.getText().toString());
+                    Log.d("tag_false",Long.toString(result));
+                    results_field.setText("0");
+                }
+
+               results_field.setText(Long.toString(result));
+
             }
         });
         final Button button_substract = (Button) findViewById(R.id.button_substract);
@@ -116,7 +210,14 @@ public class MainCalcScreen extends AppCompatActivity {
                 results_field.setText("");
             }
         });
+        final Button button_equals = (Button) findViewById(R.id.button_equals);
+        button_equals.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
+                results_field.setText(Long.toString(result));
+                operator = false;
+            }
+        });
 
 
 
